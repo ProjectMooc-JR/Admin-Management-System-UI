b005c9fbac1c41be749f107c1728d32d21528788
Zhongqi Wei <Zhongqi.Wei@outlook.com>
2024-09-01 16:50:26 +0800

    Create comment list and adding comment function

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Mainlayout from "./components/mainlayout/index";
 import { Toaster } from "react-hot-toast";
 import User from "./pages/users/index";
 import AddUser from "./pages/users/adduser";
+import Comment from "./pages/comments/index";
+import AddComment from "./pages/comments/addcomment";
 
 import { theme } from "./theme"
 import NeedAuth from "./components/NeedAuth";
@@ -21,6 +23,8 @@ function App() {
           <Route path="/dashboard" element={<NeedAuth> <Dashboard /></NeedAuth>} />
           <Route path="/users" element={<NeedAuth><User /></NeedAuth>} />
           <Route path="/users/adduser/:id?" element={<NeedAuth><AddUser /></NeedAuth>} />
+          <Route path="/comments" element={<NeedAuth>< Comment/></NeedAuth>} />
+          <Route path="/comments/addcomment/:id?" element={<NeedAuth><AddComment /></NeedAuth>} />
         </Route>
       </Routes>
     </ThemeProvider>
---
+++ src/pages/comments/commentlist.jsx
@@ -0,0 +1,20 @@
+import { DataGrid } from "@mui/x-data-grid";
+
+export default function CommentList(props) {
+  return (
+    <>
+      <DataGrid
+        checkboxSelection
+        pageSizeOptions={[25, 50, 100, 150, 300]}
+        paginationMode="server"
+        rowCount={props.data.total}
+        columns={props.columns}
+        rows={props.data.items}
+        onPaginationModelChange={props.setPaginationModel}
+        onRowSelectionModelChange={(newRowSelectionModel) => {
+          props.setRowSelectionModel(newRowSelectionModel);
+        }}
+      />
+    </>
+  );
+}
